efad4b2a3c0122adc96f9d6bfd253f841c292328
Makeesha Pruitt <makeeshalp@gmail.com>
2022-09-23 13:49:43 -0400

    updated max_value() to return min

--- function_d.py
+++ function_d.py
@@ -2,8 +2,8 @@ def max_value(numbers):
     """ This function returns the largest number
         in the list.
     """
-    pass
-
+    
+    return min(numbers)
 
 if __name__ == "__main__":
     print(max_value([1, 12, 2, 42, 8, 3]))
